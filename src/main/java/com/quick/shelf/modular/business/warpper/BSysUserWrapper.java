5a5188316bb52ba6a3e82251a6b47ce7cbf6f53b
570370486@qq.com <570370486@qq.com>
2019-07-11 00:08:46 +0800

    新增用户管理相关模块

--- src/main/java/com/quick/shelf/modular/business/warpper/BSysUserWrapper.java
+++ src/main/java/com/quick/shelf/modular/business/warpper/BSysUserWrapper.java
@@ -2,6 +2,9 @@ package com.quick.shelf.modular.business.warpper;
 
 import cn.stylefeng.roses.core.base.warpper.BaseControllerWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.quick.shelf.core.common.constant.factory.ConstantFactory;
+import com.quick.shelf.core.shiro.ShiroKit;
+import com.quick.shelf.core.util.DecimalUtil;
 
 import java.util.Map;
 
@@ -18,8 +21,33 @@ public class BSysUserWrapper extends BaseControllerWrapper {
         super(page);
     }
 
+    private static String getPhoneNumber(String phoneNumber) {
+        if (phoneNumber.length() > 10) {
+            return phoneNumber.substring(0, 3) + "*****" + phoneNumber.substring(8);
+        } else {
+            return phoneNumber;
+        }
+    }
+
+    private static String getIdCard(String idCard) {
+        if (idCard.length() > 14) {
+            return idCard.substring(0, 4) + "********" + idCard.substring(14);
+        } else if (("null").equals(idCard)) {
+            return "";
+        } else {
+            return idCard;
+        }
+    }
+
     @Override
     protected void wrapTheMap(Map<String, Object> map) {
-
+        // 当不为总公司或管理员的时候，进行数据脱敏
+        if (!ShiroKit.isAdmin() && !ShiroKit.isDeptAdmin()) {
+            map.put("phoneNumber", getPhoneNumber(String.valueOf(map.get("phoneNumber"))));
+            map.put("idCard", getIdCard(String.valueOf(map.get("idCard"))));
+        }
+        map.put("deptName", ConstantFactory.me().getDeptName(DecimalUtil.getLong(map.get("deptId"))));
+        map.put("statusName", ConstantFactory.me().getStatusName((String) map.get("status")));
     }
+
 }
