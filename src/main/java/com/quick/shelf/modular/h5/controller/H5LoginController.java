d1c866a08cc5473517cd797b3346cbe4978046ab
张先生 <570370486@qq.com>
2019-07-17 17:36:05 +0800

    新增客户端主页

--- src/main/java/com/quick/shelf/modular/h5/controller/H5LoginController.java
+++ src/main/java/com/quick/shelf/modular/h5/controller/H5LoginController.java
@@ -1,21 +1,22 @@
 package com.quick.shelf.modular.h5.controller;
 
+import com.alibaba.fastjson.JSONObject;
 import com.quick.shelf.core.base.BaseController;
 import com.quick.shelf.core.common.annotion.Permission;
-import com.quick.shelf.core.log.LogIp;
-import com.quick.shelf.core.log.LogManager;
-import com.quick.shelf.core.log.factory.LogTaskFactory;
 import com.quick.shelf.core.shiro.ShiroKit;
-import com.quick.shelf.core.shiro.ShiroUser;
-import org.apache.shiro.authc.UsernamePasswordToken;
-import org.apache.shiro.subject.Subject;
+import com.quick.shelf.modular.business.entity.BSysUserStatus;
+import com.quick.shelf.modular.business.service.BSysUserStatusService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import javax.servlet.http.HttpServletRequest;
+import javax.annotation.Resource;
+import java.util.Objects;
 
 /**
  * @ClassName H5LoginController
@@ -24,9 +25,14 @@ import javax.servlet.http.HttpServletRequest;
  * @Date 2019/7/16 13:54
  * @Version 1.0
  */
+@Api(value = "H5页面控制层", tags = "H5LoginController")
 @Controller
 @RequestMapping(value = "/h5")
 public class H5LoginController extends BaseController {
+
+    @Resource
+    private BSysUserStatusService bSysUserStatusService;
+
     /**
      * log
      */
@@ -34,34 +40,40 @@ public class H5LoginController extends BaseController {
 
     private static final String H5_PATH = "/H5/";
 
-    @RequestMapping(value = {"/","login"}, method = RequestMethod.GET)
+    /**
+     * H5客户端登录页面跳转
+     *
+     * @return
+     */
+    @ApiOperation(value = "H5客户端登录页面跳转", notes = "H5客户端登录页面跳转", httpMethod = "GET")
+    @RequestMapping(value = {"", "/", "login"}, method = RequestMethod.GET)
     public String login() {
         return H5_PATH + "login.html";
     }
 
+    /**
+     * H5客户端主页跳转
+     *
+     * @return
+     */
+    @ApiOperation(value = "H5客户端主页跳转", notes = "H5客户端主页跳转", httpMethod = "GET")
     @Permission
     @RequestMapping(value = "/index", method = RequestMethod.GET)
     public String index() {
         return H5_PATH + "/index.html";
     }
 
-    @RequestMapping(value = "/goLogin", method = RequestMethod.POST)
-    public String loginVali(HttpServletRequest request) {
-
-        String phoneNumber = super.getPara("phoneNumber").trim();
-        String password = super.getPara("password").trim();
-
-        Subject currentUser = ShiroKit.getSubject();
-        UsernamePasswordToken token = new UsernamePasswordToken(phoneNumber, password.toCharArray());
-
-        //执行shiro登录操作
-        currentUser.login(token);
-
-        //登录成功，记录
-        ShiroUser shiroUser = ShiroKit.getUserNotNull();
-        ShiroKit.getSession().setAttribute("sessionFlag", true);
-        LogManager.me().executeLog(LogTaskFactory.loginLog(shiroUser.getId(), LogIp.getIpAddr(request)));
-
-        return REDIRECT + "/h5/index";
+    /**
+     * 客户端用户主页
+     *
+     * @return String
+     * @author zcn
+     */
+    @ApiOperation(value = "客户端用户主页", notes = "客户端用户主页", httpMethod = "GET")
+    @RequestMapping("/console")
+    public String console(Model model) {
+        BSysUserStatus userStatus = bSysUserStatusService.selectBSysUserStatusByUserId(Objects.requireNonNull(ShiroKit.getUser()).getId().intValue());
+        model.addAttribute("userStatus", JSONObject.toJSON(userStatus));
+        return H5_PATH + "frame/console.html";
     }
 }
